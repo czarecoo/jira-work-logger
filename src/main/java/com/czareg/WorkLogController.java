bed13dd3b3f3a84f2d8ed5e1ec5fcac07caafea3
Cezary Witkowski <czarecoo@gmail.com>
2021-03-03 19:26:03 +0100

    Get issue works

--- src/main/java/com/czareg/WorkLogController.java
+++ src/main/java/com/czareg/WorkLogController.java
@@ -1,30 +1,23 @@
 package com.czareg;
 
-import com.atlassian.jira.rest.client.api.JiraRestClient;
-import com.atlassian.jira.rest.client.api.JiraRestClientFactory;
-import com.atlassian.jira.rest.client.api.domain.Issue;
-import com.atlassian.jira.rest.client.auth.BasicHttpAuthenticationHandler;
-import com.atlassian.jira.rest.client.internal.async.AsynchronousJiraRestClientFactory;
-import io.atlassian.util.concurrent.Promise;
+import net.rcarz.jiraclient.BasicCredentials;
+import net.rcarz.jiraclient.Issue;
+import net.rcarz.jiraclient.JiraClient;
+import net.rcarz.jiraclient.JiraException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.net.URI;
-import java.net.URISyntaxException;
 
 @RestController
 public class WorkLogController {
     private static final String JIRA_URL = "https://czarecoo.atlassian.net/";
     private static final String JIRA_ADMIN_USERNAME = "czarecoo@o2.pl";
-    private static final String JIRA_ADMIN_PASSWORD = "4funrulez";
+    private static final String JIRA_ADMIN_PASSWORD = "Wo9lwEbIlX3eHEr0zsR82839";
 
     @GetMapping
-    public void get() throws URISyntaxException {
-        JiraRestClientFactory factory = new AsynchronousJiraRestClientFactory();
-        URI uri = new URI(JIRA_URL);
-        JiraRestClient client = factory.create(uri, new BasicHttpAuthenticationHandler(JIRA_ADMIN_USERNAME, JIRA_ADMIN_PASSWORD));
-        Promise<Issue> issuePromise = client.getIssueClient().getIssue("WL-1");
-        Issue issue = issuePromise.claim();
+    public void get() throws JiraException {
+        BasicCredentials creds = new BasicCredentials(JIRA_ADMIN_USERNAME, JIRA_ADMIN_PASSWORD);
+        JiraClient jira = new JiraClient(JIRA_URL, creds);
+        Issue issue = jira.getIssue("WL-1");
         System.out.println(issue.getDescription());
     }
 }
